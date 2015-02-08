18af0c969ba9b798cfb49f0bd4cb956450a60a85
sarawr <mer12014@byui.edu>
2015-02-07 19:42:30 -0800

    Team meeting links

---
+++ team-meetings-L04.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <title>Team Meetings Lesson 03</title>
+        <meta charset="UTF-8">
+    </head>
+    <body>
+        <p>The link for our team meeting this week: 
+            <a href="https://www.youtube.com/watch?v=HoJXnsOqmG0">Team Meeting Lesson 04</a>
+        </p>
+        <p>I started talking about JavaScript Objects at 22:00.
+    </body>
+</html>
