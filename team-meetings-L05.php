ef2234dfbb3f4254c6033d1f60ded49bcb9a224f
sarawr <mer12014@byui.edu>
2015-02-14 23:14:29 -0800

    Team Meeting Lesson 5

---
+++ team-meetings-L05.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <title>Team Meetings Lesson 03</title>
+        <meta charset="UTF-8">
+    </head>
+    <body>
+        <p>The link for our team meeting this week: 
+            <a href="https://www.youtube.com/watch?v=FGFS3UgOo0Q">Team Meeting Lesson 05</a>
+        </p>
+        <p>I started talking about DOM Manipulation at 1:20</p>
+        <p>The link for my sharing video: 
+            <a href="https://www.youtube.com/watch?feature=player_embedded&v=tX36qEXh_T8">DOM Manipulation</a>
+        </p>
+    </body>
+</html>
