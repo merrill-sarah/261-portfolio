370850dd655b33662f7678e4ed327cb5945fa2aa
sarawr <mer12014@byui.edu>
2015-02-21 23:29:07 -0800

    team meeting video link for Lesson 6

---
+++ team-meetings-L06.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Team Meetings Lesson 03</title>
+        <meta charset="UTF-8">
+    </head>
+    <body>
+        <p>The link for our team meeting this week: 
+            <a href="https://www.youtube.com/watch?v=wqM8h-jJYhc">Team Meeting Lesson 06</a>
+        </p>
+        <p>I started talking about local storage at 13:20</p>
+        <p>We discussed Agile vs. Waterfall at 15:25</p>
+    </body>
+</html>
