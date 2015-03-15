99ce7d66d6b21b3c33579ce37faa16fbcec90a6a
sarawr <mer12014@byui.edu>
2015-03-14 19:36:19 -0700

    added pages

---
+++ shopping-list/index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Shopping List</title>
+        <link href="stylesheet.css" type="text/css" rel="stylesheet" />
+    </head>
+    <body>
+        <header>
+            
+        </header>
+        <main>
+            <table>
+                <tr>
+                    <td>Star</td>
+                    <td>Item 1</td>
+                    <td>Qty.</td>
+                    <td><input type="checkbox" name="check" value="item1"></td>
+                </tr>
+                <tr>
+                    <td>Star</td>
+                    <td>Item 2</td>
+                    <td>Qty.</td>
+                    <td><input type="checkbox" name="check" value="item2"></td>
+                </tr>
+                <tr>
+                    <td>Star</td>
+                    <td>Item 3</td>
+                    <td>Qty.</td>
+                    <td><input type="checkbox" name="check" value="item3"></td>
+                </tr>
+                <tr>
+                    <td>Star</td>
+                    <td>Item 4</td>
+                    <td>Qty.</td>
+                    <td><input type="checkbox" name="check" value="item4"></td>
+                </tr>
+            </table>
+        </main>
+    </body>
+</html>
